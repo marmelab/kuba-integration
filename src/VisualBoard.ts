6b1bd3f9a402976cce10c350e1cb67e153cf3f4c
Anthony Rimet <anthony.rimet03@gmail.com>
2021-08-30 16:25:21 +0200

    Add color dot

--- src/VisualBoard.ts
+++ src/VisualBoard.ts
@@ -10,7 +10,21 @@ export function render(board: Array<Array<number>>) {
     for (let i = 0; i < board.length; i++) {
         let line = ` ${i} `;
         for (const value of board[i]) {
-            line += ` ${value} `
+            switch (value) {
+                case 1:
+                    line += `\u001b[31m \u2022 \u001b[0m`
+                    break;
+                case 2:
+                    line += `\u001b[34m \u2022 \u001b[0m`
+                    break;
+
+                case 3:
+                    line += `\u001b[37m \u2022 \u001b[0m`
+                    break;
+                default:
+                    line += `   `
+            }
+
         }
         console.log(line)
     }

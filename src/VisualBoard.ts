4aab78ce7972c1bfc30ce1c93efc36b1da6ba535
Anthony Rimet <anthony.rimet03@gmail.com>
2021-08-30 17:05:24 +0200

    Fix typo

--- src/VisualBoard.ts
+++ src/VisualBoard.ts
@@ -9,15 +9,14 @@ export function buildGraphicalBoard(board: Array<Array<number>>): String {
 
     for (let i = 0; i < board.length; i++) {
         let line = ` ${i} `;
-        for (const value of board[i]) {
-            switch (value) {
+        for (const marble of board[i]) {
+            switch (marble) {
                 case 1:
                     line += `\u001b[31m \u2022 \u001b[0m`
                     break;
                 case 2:
                     line += `\u001b[34m \u2022 \u001b[0m`
                     break;
-
                 case 3:
                     line += `\u001b[37m \u2022 \u001b[0m`
                     break;

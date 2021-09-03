c24d379200d2126bb1283e64b4823dfa058e13b5
Anthony Rimet <anthony.rimet03@gmail.com>
2021-09-03 09:56:49 +0200

    add cardinal box

--- src/blessed.ts
+++ src/blessed.ts
@@ -26,16 +26,48 @@ export const renderScreenView = (gameState: GameState) => {
     height: 40,
   });
 
+  const cardinalEastBox = blessed.box({
+    top: "50%",
+    left: "90%",
+    width: 2,
+    height: 1,
+    content: "\u25bA",
+  });
+
+  const cardinalWestBox = blessed.box({
+    top: "50%",
+    left: "10%",
+    content: "\u25c4",
+    width: 2,
+    height: 1,
+  });
+
+  const cardinalNorthBox = blessed.box({
+    top: "90%",
+    left: "50%",
+    content: "\u25bc",
+    width: 2,
+    height: 1,
+  });
+
+  const cardinalSouthBox = blessed.box({
+    top: "10%",
+    left: "50%",
+    content: "\u25b2",
+    width: 2,
+    height: 1,
+  });
+
   const playerTurnText = blessed.box({
     top: "center",
-    left: "90%",
+    left: "80%",
     tags: true,
     content: `Player turn : ${gameState.currentPlayer.playerNumber}`,
   });
 
   const board = blessed.box({
     top: "center",
-    left: "left",
+    left: "center",
     width: 56, //*8
     height: 28, // *4
     tags: true,
@@ -55,9 +87,10 @@ export const renderScreenView = (gameState: GameState) => {
       let tmpBox = blessed.box({
         top: node.y * 4,
         left: node.x * 8,
-        width: 7,
-        height: 3,
-        content: node.value === 0 ? "" : "\u2022",
+        width: 6,
+        height: 2,
+        content: node.value === 0 ? "" : "{center}\u2022{/center}",
+        tags: true,
         style: {
           fg: MARBLE_INT_COLORS[node.value],
           bg: (gameState.marbleClicked === node) ? 'yellow' : '',
@@ -72,7 +105,15 @@ export const renderScreenView = (gameState: GameState) => {
   });
 
   SCREEN.append(outerBoard);
-  outerBoard.append(playerTurnText);
   outerBoard.append(board);
+  outerBoard.append(cardinalEastBox);
+  outerBoard.append(cardinalWestBox);
+  outerBoard.append(cardinalNorthBox);
+  outerBoard.append(cardinalSouthBox);
+
+
+  SCREEN.append(playerTurnText);
+  
+  
   SCREEN.render();
 };

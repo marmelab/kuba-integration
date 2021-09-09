03485f2fc74895be8dcf3a9ac085bdbc396b41d1
Alexandre Herbeth <alexandre.herbeth@gmail.com>
2021-09-09 14:38:06 +0200

    login route

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -12,12 +12,14 @@ import { AppService } from './app.service';
 import { initializePlayers } from './game';
 import { GameState, Player } from './types';
 import { GameService } from './game.service';
+import { UserService } from './user.service';
 import { INITIAL_BOARD } from './constants';
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly gameService: GameService,
+    private readonly userService: UserService,
     private gatewayService: AppGateway,
   ) {}
 
@@ -137,9 +139,60 @@ export class AppController {
     return this.gameService.deserializer(res);
   }
 
-  @Post('createAppAdmin')
-  async createAppAdmin(
+  @Post('createuser')
+  async createuser(
     @Body('username') username: string,
     @Body('passhash') passhash: string,
-  ) {}
+  ) {
+    if (!username || !passhash) {
+      throw new HttpException("Something's wrong with your credentials ", 400);
+    }
+  }
+
+  @Post('createadmin')
+  async createAdmin() {
+    try {
+      if (await this.userService.getUser({ email: 'adm@mrmlb.com' })) {
+        throw new HttpException(
+          "this is not the droids you're looking for...",
+          400,
+        );
+      }
+      return this.userService.createUser({
+        email: 'adm@mrmlb.com',
+        hash: '$2y$10$232.rKLBowZhyb7QxkiJkeSar3lKR4fKL8y/34w2oOM9t.eyUnRUm',
+      });
+    } catch (e) {
+      throw new HttpException(
+        "this is not the droids you're looking for...",
+        400,
+      );
+    }
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('hash') hash: string,
+  ): Promise<string> {
+    console.log(`email`, email);
+    console.log(`hash`, hash);
+    try {
+      const user = await this.userService.getUser({ email });
+      if (!user) {
+        throw new HttpException(
+          "Something's wrong with your credentials ",
+          400,
+        );
+      }
+
+      if (user.hash === hash) {
+        return "Well done, you're connected !";
+      }
+    } catch (e) {
+      throw new HttpException("Something's wrong with your credentials ", 400);
+    }
+
+    return 'NOK';
+  }
 }

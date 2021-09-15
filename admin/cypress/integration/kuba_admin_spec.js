4d5ea5fadfb448018f18ef15b163621bb136ad82
Anthony Rimet <anthony.rimet03@gmail.com>
2021-09-15 17:27:31 +0200

    replace exist with be.visible

--- admin/cypress/integration/kuba_admin_spec.js
+++ admin/cypress/integration/kuba_admin_spec.js
@@ -8,27 +8,27 @@ describe("Kuba Admin Test", () => {
     it("with bad email and no password should display error message", () => {
       cy.visit(Cypress.env("ADMIN_URL"));
       cy.findByRole("button", { name: /Create/i }).click();
-      cy.findByText("Create User").should("exist");
-      cy.get("#email").should("exist");
+      cy.findByText("Create User").should("be.visible");
+      cy.get("#email").should("be.visible");
       cy.get("#email").type("NotAnEmail");
-      cy.get("#password").should("exist");
+      cy.get("#password").should("be.visible");
       cy.findByRole("button", { name: /Save/i }).click();
-      cy.findByText("Must be a valid email").should("exist");
-      cy.findByText("Required").should("exist");
+      cy.findByText("Must be a valid email").should("be.visible");
+      cy.findByText("Required").should("be.visible");
       cy.findByText("The form is not valid. Please check for errors").should(
-        "exist"
+        "be.visible"
       );
     });
 
     it("with good credentials should display a success message", () => {
       cy.visit(Cypress.env("ADMIN_URL"));
       cy.findByRole("button", { name: /Create/i }).click();
-      cy.findByText("Create User").should("exist");
-      cy.get("#email").should("exist");
+      cy.findByText("Create User").should("be.visible");
+      cy.get("#email").should("be.visible");
       cy.get("#email").type(cy.faker.internet.email());
       cy.get("#password").type("password");
       cy.findByRole("button", { name: /Save/i }).click();
-      cy.findByText("Element created").should("exist");
+      cy.findByText("Element created").should("be.visible");
     });
   });
 
@@ -36,12 +36,12 @@ describe("Kuba Admin Test", () => {
     it("should display a success message", () => {
       cy.visit(Cypress.env("ADMIN_URL"));
       cy.get('tr[resource="user"]').eq(0).get("td").eq(1).click();
-      cy.findByText(/User #/).should("exist");
-      cy.get("#email").should("exist");
+      cy.findByText(/User #/).should("be.visible");
+      cy.get("#email").should("be.visible");
       cy.get("#email").clear();
       cy.get("#email").type(cy.faker.internet.email());
       cy.findByRole("button", { name: /Save/i }).click();
-      cy.findByText("Element updated").should("exist");
+      cy.findByText("Element updated").should("be.visible");
     });
   });
 
@@ -50,7 +50,7 @@ describe("Kuba Admin Test", () => {
       cy.visit(Cypress.env("ADMIN_URL"));
       cy.findAllByRole("checkbox").eq(1).click();
       cy.findByRole("button", { name: /Delete/i }).click();
-      cy.findByText("Element deleted").should("exist");
+      cy.findByText("Element deleted").should("be.visible");
     });
   });
 });

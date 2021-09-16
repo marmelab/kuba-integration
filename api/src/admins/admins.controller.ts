e7cb9a9d45b172610eda3a4558836d778ab786a7
Alexandre Herbeth <alexandre.herbeth@gmail.com>
2021-09-16 10:35:08 +0200

    admins api added

---
+++ api/src/admins/admins.controller.ts
@@ -0,0 +1,119 @@
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Query,
+  HttpException,
+  Post,
+  Body,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Admin } from 'src/types';
+import { AdminsService } from './admins.service';
+import { ADMIN_TYPE } from 'src/constants';
+
+@Controller('admins')
+export class AdminsController {
+  constructor(private readonly adminsService: AdminsService) {}
+
+  @Get(':id')
+  async getAdmin(@Param('id', ParseIntPipe) id: number): Promise<Admin> {
+    try {
+      return await this.adminsService.getAdmin({ id });
+    } catch (error) {
+      throw new NotFoundException(`This admin doesn't exists`);
+    }
+  }
+
+  @Get()
+  async getAdmins(
+    @Query('filter') filter: string,
+    @Query('sort') sort: string,
+    @Query('range') range: string,
+  ) {
+    let params = {};
+
+    if (filter) {
+      try {
+        const filters = JSON.parse(filter);
+        if (filters.email) {
+          params = {
+            where: {
+              email: {
+                contains: filters.email,
+              },
+            },
+          };
+        }
+      } catch (e) {
+        throw new HttpException('Incorrect filter parameter', 400);
+      }
+    }
+
+    if (range) {
+      try {
+        const rangeNumber = JSON.parse(range) as number[];
+        if (rangeNumber) {
+          params = {
+            ...params,
+            take: rangeNumber[1] - rangeNumber[0],
+            skip: rangeNumber[0],
+          };
+        }
+      } catch (e) {
+        throw new HttpException('Incorrect range parameter', 400);
+      }
+    }
+
+    if (sort) {
+      try {
+        const sortParsed = JSON.parse(sort);
+        if (sortParsed) {
+          params = {
+            ...params,
+            orderBy: {
+              [sortParsed[0]]: sortParsed[1].toLowerCase(),
+            },
+          };
+        }
+      } catch (e) {
+        throw new HttpException('Incorrect sort parameter', 400);
+      }
+    }
+
+    const users = await this.adminsService.getAdmins(params);
+    return users;
+  }
+
+  @Post('create-admin')
+  async createAdmin(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    if (!email || !password) {
+      throw new HttpException("Something's wrong with your credentials ", 400);
+    }
+    return this.adminsService.createAdmin(email, password);
+  }
+
+  @Post('setup-admin')
+  async setupAdmin() {
+    const superAdmin = await this.adminsService.getAdmin({
+      email: 'adm@mrmlb.com',
+    });
+
+    if (superAdmin) {
+      throw new ForbiddenException(
+        "These are not the droïds you're looking for",
+      );
+    }
+
+    return this.adminsService.createAdmin(
+      'adm@mrmlb.com',
+      '1234',
+      ADMIN_TYPE.SUPER_ADMIN,
+    );
+  }
+}

bb3afcffe144b4f37efe06f6a306d9cfb8dab683
Alexandre Herbeth <alexandre.herbeth@gmail.com>
2021-09-16 16:48:17 +0200

    basic RBAC for admins/ super admins

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -3,17 +3,21 @@ import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
 import { UserCreate, UserEdit, UserList } from "./User";
 import { AdminList, AdminCreate, AdminEdit } from "./Admins";
+import { ADMIN_TYPE } from "./constants";
 
 
-const App = () => {
-  
-  return (
+const App = () => (
     <Admin dataProvider={dataProvider} authProvider={authProvider as any}>
-    <Resource name="user" list={UserList} create={UserCreate} edit={UserEdit} />
-    <Resource name="admins" list={AdminList} create={AdminCreate} edit={AdminEdit} /> 
-  </Admin>
-  
+    
+    {
+      permissions => {
+        return [
+        <Resource name="user" list={UserList} create={UserCreate} edit={UserEdit} />,
+        permissions === ADMIN_TYPE.SUPER_ADMIN ? <Resource name="admins" list={AdminList} create={AdminCreate} edit={AdminEdit} /> : null
+      ]}
+    }
+    </Admin>
 );
-}
+
 
 export default App;
